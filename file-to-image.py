1b95d97e13b4d9fe77e6b5cfa3b9c8cd177c9325
Michael <99michaelrussell99@gmail.com>
2024-07-07 12:38:35 -0400

    Script now works with any file type

--- file-to-image.py
+++ file-to-image.py
@@ -6,14 +6,13 @@ import numpy as np
 
 from PIL import Image, ImageColor
 
-def encode_text(filename, rainbow):
-    with open(filename, 'r',encoding='utf8') as file:
-        text_string = file.read()
-
-    text_binary = ''.join(format(ord(i),'b').zfill(8) for i in text_string)
+def encode_file(filename, rainbow):
+    with open(filename, 'rb') as file:
+        binary_content = file.read()
+    text_binary = ''.join(format(byte, '08b') for byte in binary_content)
 
     side_length = get_square_size(len(text_binary))
-    img = Image.new('RGB', (side_length,side_length), color = ImageColor.getrgb("white"))
+    img = Image.new('RGB', (side_length,side_length), color = (254,254,254))
 
     current_pixel = 0
     for i in range(side_length):
@@ -27,7 +26,7 @@ def encode_text(filename, rainbow):
                     if(rainbow):
                         while (True):
                             color = (random.randint(0,256),random.randint(0,256),random.randint(0,256))
-                            if(color != ImageColor.getrgb("white")):
+                            if(color != ImageColor.getrgb("white") and color != (254,254,254)):
                                 break
                     else:
                         color = ImageColor.getrgb("black")
@@ -35,44 +34,35 @@ def encode_text(filename, rainbow):
                     current_pixel += 1
 
     extension = os.path.splitext(filename)[1][1:]
-    img.save(filename.replace(extension,"png"))
+    filename_no_extension = filename.split('.')[0]
+    img.save(filename.replace(extension,"png").replace(filename_no_extension,filename_no_extension + "_converted"))
 
 def decode_img(filename):
     img = Image.open(filename)
     numpy_array = np.array(img)
     text_binary = ""
-    text_binary_array = []
-    byte_count = 0
 
     white = np.array([255, 255, 255])
+    end = np.array([254, 254, 254])
 
     for line in numpy_array:
         for pixel in line:
-            if(byte_count == 8):
-                if(text_binary == "00000000"):
-                    return binary_array_to_string(text_binary_array)
-
-                byte_count = 0
-                text_binary_array.append(text_binary)
-                text_binary = ""
-
-            if(np.array_equal(pixel, white)):
-                byte_count += 1
+            if(np.array_equal(pixel, end)):
+               return text_binary
+            elif(np.array_equal(pixel, white)):
                 text_binary += '0'
             else:
-                byte_count += 1
                 text_binary += '1'
 
-def binary_array_to_string(binary_array):
-    characters = []
-    for b in binary_array:
-        character = chr(int(b, 2))
-        if ord(character) < 128:
-            characters.append(character)
-        else:
-            characters.append('?')
-
-    return ''.join(characters)
+def binary_string_to_file(binary_string, filename):
+    byte_array = bytearray()
+    
+    for i in range(0, len(binary_string), 8):
+        byte = binary_string[i:i+8]
+        byte_array.append(int(byte, 2))
+    
+    with open(filename, 'wb') as file:
+        file.write(byte_array)
 
 def get_square_size(x):
     s = math.ceil(math.sqrt(x))
@@ -82,14 +72,15 @@ def get_square_size(x):
 
 parser = argparse.ArgumentParser(description='file-to-image')
 parser.add_argument('-e',type=str, help="file to be encoded")
-parser.add_argument('-d',type=str, help="file to be decoded")
+parser.add_argument('-d',type=str, nargs=2, help="[file to be decoded] [original filetype]")
 parser.add_argument('-r',action=argparse.BooleanOptionalAction, help="enable rainbow")
 args = parser.parse_args()
 
 if(args.e and args.r):
-    encode_text(args.e, True)
+    encode_file(args.e, True)
 if(args.e and not args.r):
-    encode_text(args.e, False)
+    encode_file(args.e, False)
 if(args.d):
-    with open("output.txt", "w") as output_file:
-        output_file.write(decode_img(args.d))
+    orig_filename = args.d[0].split("_converted")[0] + "_original." + args.d[1]
+    binary_string  = decode_img(args.d[0])
+    binary_string_to_file(binary_string, orig_filename)
